c63ed7bdd6e2d90ef34b4aac749375f9b5fb23b9
Filippo Brigati <filippobrigati2@gmail.com>
2025-06-06 11:25:57 +0200

    Add filter and suggestion components to sidebar; update sidebar template for dynamic rendering

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,18 +1,35 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { LucideAngularModule, User, LogOut } from 'lucide-angular';
 import { Auth as AuthService } from '../../core/service/auth';
+import { Router, Event, NavigationEnd, NavigationError } from '@angular/router';
+import { Filter } from './components/filter/filter';
+import { Suggestion } from './components/suggestion/suggestion';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, Filter, Suggestion],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
 export class Sidebar {
   private readonly service = inject(AuthService);
+  private readonly router = inject(Router);
 
   protected readonly UserIcon = User;
   protected readonly LogoutIcon = LogOut;
+
+  currentRoute = signal<string>('');
+
+  constructor() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute.set(event.url.split('?')[0]);
+      }
+      if (event instanceof NavigationError) {
+        console.error(event.error);
+      }
+    });
+  }
 
   isAuth(): boolean {
     return this.service.isLoggedIn();
