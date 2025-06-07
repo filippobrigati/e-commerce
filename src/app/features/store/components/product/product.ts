13854e91066518ced48828dc5d713e7172e4ad99
filippobrigati <filippobrigati2@gmail.com>
2025-06-07 11:39:11 +0200

    feat: integrate angular-cli-ghpages for deployment

--- src/app/features/store/components/product/product.ts
+++ src/app/features/store/components/product/product.ts
@@ -1,6 +1,7 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output, signal } from '@angular/core';
 import { Product as ProductType } from '../../../../core/model/products';
 import { CurrencyPipe } from '@angular/common';
+import { Store as StoreService } from '../../service/store';
 
 @Component({
   selector: 'app-product',
@@ -9,7 +10,22 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './product.css'
 })
 export class Product {
+  private readonly service = inject(StoreService);
+
   product = input.required<ProductType>();
 
   navigate = output<number>();
+  sending = signal<boolean>(false);
+
+  sendToCart($event: Event): void {
+    $event.stopPropagation();
+
+    this.sending.set(true);
+
+    setTimeout(() => {
+      this.service.addToCart(this.product());
+
+      this.sending.set(false);
+    }, 2000);
+  }
 }
--- src/app/features/store/service/store.ts
+++ src/app/features/store/service/store.ts
@@ -3,12 +3,14 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../../../core/model/products';
 import { environment } from '../../../../environments/environment';
+import { Cart as CartService } from '../../../core/service/cart/cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Store {
   private http = inject(HttpClient);
+  private readonly service = inject(CartService);
 
   /**
    * Get all products from api
@@ -17,5 +19,9 @@ export class Store {
    */
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${environment.apiURL}/products`);
+  }
+
+  addToCart(product: Product, quantity: number = 1): void {
+    this.service.add(product, quantity);
   }
 }
